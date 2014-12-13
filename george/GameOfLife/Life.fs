4e8a54fadba2eeecc47ef43d957c4d47703bc9d0
George Pollard <porges@porg.es>
2014-12-14 12:47:42 +1300

    Add my implementation

---
+++ george/GameOfLife/Life.fs
@@ -0,0 +1,176 @@
+﻿module Life 
+    
+open System
+
+// A cell is either alive or dead
+type Cell = Dead | Alive
+
+// A point is x & y coordinates
+type Point = 
+    struct
+        val x : int
+        val y : int
+    end
+    new(x : int, y : int) = { x = x; y = y; }
+
+    // We can add or subtract points:
+    static member (+) (p1 : Point, p2 : Point) = Point (p1.x + p2.x, p1.y + p2.y)
+    static member (-) (p1 : Point, p2 : Point) = Point (p1.x - p2.x, p1.y - p2.y)
+
+    override this.ToString() = sprintf "(%d, %d)" this.x this.y
+
+// A board is an array of arrays of cells
+type Board = 
+    private
+    | Board of Cell[,]
+
+    static member mapPoints f (Board b) = seq {
+        let minY = Array2D.base1 b
+        let maxY = Array2D.length1 b + minY - 1
+        let minX = Array2D.base2 b
+        let maxX = Array2D.length2 b + minX - 1 
+        
+        for y in [minY..maxY] do
+            for x in [minX..maxX] do
+                yield f (Point(x, y))
+        }
+
+    static member iterPoints f (Board b) =
+        let minY = Array2D.base1 b
+        let maxY = Array2D.length1 b + minY - 1
+        let minX = Array2D.base2 b
+        let maxX = Array2D.length2 b + minX - 1 
+
+        for y in [minY..maxY] do
+            for x in [minX..maxX] do
+                f (Point(x, y))
+
+    static member iterPointsValues f (Board b) =
+        let minY = Array2D.base1 b
+        let maxY = Array2D.length1 b + minY - 1
+        let minX = Array2D.base2 b
+        let maxX = Array2D.length2 b + minX - 1 
+
+        for y in [minY..maxY] do
+            for x in [minX..maxX] do
+                f (Point(x, y)) b.[y, x]
+        
+let width (Board b) = Array2D.length2 b
+let height (Board b) = Array2D.length1 b
+let fromCells = Board << Array2D.copy // we want to know that only we can touch the array, so copy it
+let topLeft (Board b) = Point(Array2D.base2 b, Array2D.base1 b)
+let bottomRight (Board b) = Point(Array2D.base2 b + Array2D.length2 b, Array2D.base1 b + Array2D.length1 b)
+    
+// Check if a point is valid on a board:
+let private inBounds b (p : Point) = 
+    p.y < (Array2D.length1 b + Array2D.base1 b) && p.y >= Array2D.base1 b &&
+    p.x < (Array2D.length2 b + Array2D.base2 b) && p.x >= Array2D.base2 b
+
+// We can access the point on a board (safely)
+let (@) (Board b) p = if inBounds b p then b.[p.y, p.x] else Dead
+
+// All points currently available on the board.
+let points = Board.mapPoints id
+
+// We can check if a particular cell is alive
+let private isAlive board point = board @ point = Alive
+
+// Get all the neighbours for a particular point:
+let private neighbours p = seq { 
+    for y in [-1..1] do
+      for x in [-1..1] do
+        if not (x = 0 && y = 0) 
+          then yield p + Point(x, y)
+    }
+
+// Count the number of neighbours that are alive for a point on a board:
+let private aliveNeighbours b = neighbours >> Seq.sumBy (isAlive b >> Convert.ToInt32)
+
+// Calculate the new state for a particular point:
+let private newState b p = 
+    if isAlive b p 
+      then 
+        match aliveNeighbours b p
+          with
+          | 2 | 3 -> Alive
+          | _ -> Dead
+      else
+        match aliveNeighbours b p
+         with
+         | 3 -> Alive
+         | _ -> Dead
+
+// Copies a board with new boundaries (anything outside original board is considered dead)
+let private withBounds b i1 i2 j1 j2 = 
+    Board <| Array2D.initBased i1 j1 (i2-i1+1) (j2-j1+1)
+        (fun i j -> if inBounds b (Point(j, i)) then b.[i, j] else Dead)
+
+let private orElse v = function
+    | Some v' -> v'
+    | None -> v
+
+let private existsCol j p a = 
+    let startI = Array2D.base1 a
+    let endI = Array2D.length1 a + startI - 1
+
+    let rec loopI i = i < endI && (p a.[i, j] || loopI (i+1))
+    loopI startI
+
+let private existsRow i p a = // delicious
+    let startJ = Array2D.base2 a
+    let endJ = Array2D.length2 a + startJ - 1
+
+    let rec loopJ j = j < endJ && (p a.[i, j] || loopJ (j+1))
+    loopJ startJ
+
+// Adjusts a board so that it will always have 'room to grow'
+let private adjustBounds (Board b) =
+    let isColAlive i = existsCol i (fun s -> s = Alive) b
+    let isRowAlive i = existsRow i (fun s -> s = Alive) b
+
+    let firstX = Array2D.base2 b
+    let lastX = Array2D.length2 b + firstX - 1
+    let firstY = Array2D.base1 b
+    let lastY = Array2D.length1 b + firstY - 1
+
+    let minX = [firstX..lastX] |> Seq.tryFind isColAlive |> orElse firstX
+    let maxX = [lastX.. -1 ..firstX] |> Seq.tryFind isColAlive |> orElse firstX
+    let minY = [firstY..lastY] |> Seq.tryFind isRowAlive |> orElse firstY
+    let maxY = [lastY.. -1 ..firstY] |> Seq.tryFind isRowAlive |> orElse firstY
+    withBounds b (minY-1) (maxY+1) (minX-1) (maxX+1)
+
+type IBoardDiffer =
+    abstract cellChanged : Point -> Cell -> unit
+
+// Mutably builds a board from an old one and a diff:
+type private BoardBuilder (basedOn : Board) = 
+    let (Board b) = basedOn
+    let myBoard = Array2D.copy b
+    member private this.set (p : Point) state = myBoard.[p.y, p.x] <- state
+    interface IBoardDiffer with
+        member this.cellChanged p s = this.set p s
+    member this.Built() = Board <| myBoard
+
+// Run 2 differs at once
+let private biDiffer (l : IBoardDiffer, r: IBoardDiffer) = { 
+        new IBoardDiffer with
+        member this.cellChanged p s = l.cellChanged p s ; r.cellChanged p s
+    } 
+
+let private actionDiffer (action : Point -> Cell -> Unit) = {
+        new IBoardDiffer with
+        member this.cellChanged p s = action p s
+    }
+
+// Generates a new board based on an old one, and emits what changed:
+// TODO: rework this to not use mutation and instead emit a list of changes
+let generateNextBoard (b : Board) (differ : Point -> Cell -> Unit) : Board = 
+    let b' = adjustBounds b
+    let builder = BoardBuilder(b')
+    let differ' = biDiffer(builder, actionDiffer(differ))
+    Board.iterPointsValues (fun p old ->
+        match newState b' p with
+            | next when next = old -> ()
+            | next -> differ'.cellChanged p next
+            ) b'
+    adjustBounds (builder.Built())
---
+++ george/GameOfLife/Main.fs
@@ -0,0 +1,165 @@
+﻿module Main
+
+open Life
+
+// Here's an example board:
+let board1 = fromCells <| array2D (List.replicate 3 [| Dead; Alive; Dead |])
+
+let glider = 
+    fromCells <| array2D [|
+        [| Dead; Alive; Dead |]
+        [| Dead; Dead; Alive |]
+        [| Alive; Alive; Alive |]
+    |]
+
+let rpentomino = 
+    fromCells <| array2D [|
+        [| Dead; Alive; Alive |]
+        [| Alive; Alive; Dead |]
+        [| Dead; Alive; Dead |]
+    |]
+
+let diehard = 
+    fromCells <| array2D [|
+        [| Dead; Dead; Dead; Dead; Dead; Dead; Alive; Dead |]
+        [| Alive; Alive; Dead; Dead; Dead; Dead; Dead; Dead |]
+        [| Dead; Alive; Dead; Dead; Dead; Alive; Alive; Alive |]
+    |]
+
+open System
+open System.Collections.Generic
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media
+open System.Windows.Media.Animation
+open System.Windows.Shapes
+
+type GraphicalBoard (c: Canvas) =
+    let boxSize = 10.0
+    let boxCenter = boxSize / 2.0
+
+    let canvasOffset = TranslateTransform()
+    let canvasScale = ScaleTransform()
+
+    let mutable myBoard = fromCells <| array2D [| [| |] |]
+    let children = Dictionary<Life.Point, Rectangle>()
+    let border = Rectangle() // Stroke = Brushes.AliceBlue if you want to see the board outline
+    let rand = Random()
+
+    do
+        let transform = TransformGroup()
+        transform.Children.Add(canvasOffset)
+        transform.Children.Add(canvasScale)
+        c.RenderTransform <- transform
+
+    member this.reCenter() =
+        let topLeft = topLeft myBoard
+        let bottomRight = bottomRight myBoard
+        let (x, y) = (float topLeft.x * boxSize, float topLeft.y * boxSize)
+        let (x', y') = (float bottomRight.x * boxSize, float bottomRight.y * boxSize)
+
+        border.Width <- (x' - x)
+        border.Height <- (y' - y)
+        Canvas.SetLeft(border, x)
+        Canvas.SetTop(border, y)
+
+        let centerX = (x' - x) / 2.0 + x
+        let centerY = (y' - y) / 2.0 + y
+
+        let offsetX = c.ActualWidth / 2.0 - centerX
+        let offsetY = c.ActualHeight / 2.0 - centerY
+        let xAnimation = DoubleAnimation(offsetX, Duration.Automatic, EasingFunction = CubicEase())
+        canvasOffset.BeginAnimation(TranslateTransform.XProperty, xAnimation)
+        let yAnimation = DoubleAnimation(offsetY, Duration.Automatic, EasingFunction = CubicEase())
+        canvasOffset.BeginAnimation(TranslateTransform.YProperty, yAnimation)
+
+        let scaleX = c.ActualWidth / (x' - x)
+        let scaleY = c.ActualHeight / (y' - y)
+        let scale = min scaleX scaleY * 0.85
+        canvasScale.CenterX <- c.ActualWidth / 2.0
+        canvasScale.CenterY <- c.ActualHeight / 2.0
+        let xAnimation = DoubleAnimation(scale, Duration.Automatic, EasingFunction = BackEase(EasingMode = EasingMode.EaseOut))
+        canvasScale.BeginAnimation(ScaleTransform.ScaleXProperty, xAnimation)
+        let yAnimation = DoubleAnimation(scale, Duration.Automatic, EasingFunction = BackEase(EasingMode = EasingMode.EaseOut))
+        canvasScale.BeginAnimation(ScaleTransform.ScaleYProperty, yAnimation) 
+
+    member this.initChildren() =
+        children.Clear()
+        c.Children.Clear()
+        c.Children.Add(border) |> ignore
+        Board.iterPointsValues (fun p s -> if s = Alive then this.addChild p else ()) myBoard
+        
+    member this.addChild (p : Life.Point) : unit =
+        let colorBase = Colors.CornflowerBlue
+        let brush = SolidColorBrush(colorBase)
+        let r = Rectangle(
+                    Fill = brush,
+                    SnapsToDevicePixels = true,
+                    Width = boxSize,
+                    Height = boxSize)
+                    
+        let appear = ScaleTransform(0.0, 0.0, CenterX = boxCenter, CenterY = boxCenter) :> Transform
+        r.RenderTransform <- appear
+        
+        let anim = DoubleAnimation(0.0, 1.0, Duration(TimeSpan.FromSeconds(0.1)), EasingFunction = BackEase(EasingMode = EasingMode.EaseOut))
+        appear.BeginAnimation(ScaleTransform.ScaleXProperty, anim)
+        anim.Completed.Add(fun _ -> if Object.ReferenceEquals(r.RenderTransform, appear) then r.RenderTransform <- null)
+        appear.BeginAnimation(ScaleTransform.ScaleYProperty, anim)
+
+        Canvas.SetLeft (r, boxSize * float p.x)
+        Canvas.SetTop (r, boxSize * float p.y)
+        
+        children.Add(p, r)
+        c.Children.Add(r) |> ignore
+
+    member private this.removeChild (p : Life.Point) : unit =
+        let child = children.[p]
+        children.Remove p |> ignore
+        
+        let shrink = ScaleTransform(CenterX = boxCenter, CenterY = boxCenter) :> Transform
+        let rotate = RotateTransform(0.0, CenterX = boxCenter, CenterY = boxCenter) :> Transform
+        let group = TransformGroup()
+        group.Children.Add(rotate)
+        group.Children.Add(shrink)
+
+        child.RenderTransform <- group
+
+        let duration = Duration(TimeSpan.FromSeconds(rand.NextDouble()/3.0+0.2))
+        let anim = DoubleAnimation(0.0, duration, EasingFunction = QuadraticEase(EasingMode = EasingMode.EaseIn))
+        shrink.BeginAnimation(ScaleTransform.ScaleXProperty, anim)
+        anim.Completed.Add(fun _ -> c.Children.Remove(child))
+        shrink.BeginAnimation(ScaleTransform.ScaleYProperty, anim)
+
+        let rotAnim = DoubleAnimation(rand.NextDouble() * 360.0 - 180.0, duration, EasingFunction = QuadraticEase(EasingMode = EasingMode.EaseIn))
+        rotate.BeginAnimation(RotateTransform.AngleProperty, rotAnim)
+
+        let colorAnim = ColorAnimation(Colors.White, duration)
+        child.Fill.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim)
+        
+
+    member private this.cellChanged (p : Life.Point) = function
+        | Dead -> this.removeChild p
+        | Alive -> this.addChild p
+
+    member this.ProcessKeyDown (keys : Input.KeyEventArgs) = 
+        myBoard <- generateNextBoard myBoard this.cellChanged
+        this.reCenter()
+
+    member this.initialize(b : Board) =
+        myBoard <- b
+        this.initChildren()
+        this.reCenter()
+
+    member this.Resized (args : SizeChangedEventArgs) =
+        this.reCenter()
+
+
+let canvas = Canvas()
+let graphical = GraphicalBoard(canvas)
+let window = Window(Content = canvas)
+window.Loaded.Add(fun _ -> graphical.initialize(diehard))
+window.KeyDown.Add(graphical.ProcessKeyDown)
+window.SizeChanged.Add(graphical.Resized)
+
+[<STAThread>]
+Application().Run window |> ignore
